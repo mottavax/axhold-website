647bc211816996526db96ac3d5c71fcb9cd5df58
mottavax <leonchinguwa@gmail.com>
2025-04-20 13:29:43 +0100

    Delete src/components directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-200 dark:bg-gray-700 py-6 text-center text-sm text-gray-600 dark:text-gray-300">
-      © {new Date().getFullYear()} Axhold. All rights reserved.
-    </footer>
-  );
-}
